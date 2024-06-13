9cf2f0b3b29d8c98613c064e04fa56388f977d2e
Zaki Ahmed <zaki.ahmed.perso@gmail.com>
2024-06-13 17:13:31 +0200

    Fix cross compilation

--- shaka-packager/cppbuild.sh
+++ shaka-packager/cppbuild.sh
@@ -46,14 +46,14 @@ case $PLATFORM in
         ;;
     linux-arm64)
         export PREFIX=aarch64-linux-gnu
-        CXX=$PREFIX-g++ CC=/$PREFIX-gcc $CMAKE $CMAKE_CONFIG -DSKIP_INTEGRATION_TESTS=ON -DCMAKE_FIND_ROOT_PATH=/usr/aarch64-linux-gnu  ..
+        CXX=$PREFIX-g++ CC=/$PREFIX-gcc $CMAKE $CMAKE_CONFIG -DSKIP_INTEGRATION_TESTS=ON -DCMAKE_CROSSCOMPILING=1 -DCMAKE_FIND_ROOT_PATH=/usr/aarch64-linux-gnu
         $CMAKE --build build/ --config Release --parallel 
         $CMAKE --install build/ --strip --config Release --prefix=$CURRENT_PLATFORM_PATH
         ;;
     macosx-arm64)
-        $CMAKE -S . -B build -DBUILD_SHARED_LIBS="ON"  -DCMAKE_BUILD_TYPE=Release -G Ninja  -DCMAKE_INSTALL_PREFIX=$CURRENT_PLATFORM_PATH
-        $CMAKE --build build/ --config Release --parallel 
-        $CMAKE --install build/ --strip --config Release --prefix=$CURRENT_PLATFORM_PATH
+        cmake -S . -B build -DBUILD_SHARED_LIBS="ON"  -DCMAKE_BUILD_TYPE=Release -G Ninja  -DCMAKE_INSTALL_PREFIX=$CURRENT_PLATFORM_PATH
+        cmake --build build/ --config Release --parallel 
+        cmake --install build/ --strip --config Release --prefix=$CURRENT_PLATFORM_PATH
         ;;
     macosx-x86_64)
         cmake -S . -B build -DBUILD_SHARED_LIBS="ON"  -DCMAKE_BUILD_TYPE=Release -G Ninja  -DCMAKE_INSTALL_PREFIX=$CURRENT_PLATFORM_PATH
